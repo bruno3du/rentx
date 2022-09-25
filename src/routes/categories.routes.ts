0577ff308b7b2ace882976a540ca3e31a9b9adc3
bruno3du <bruno.3duardo@hotmail.com>
2022-09-25 16:54:26 -0300

    initial

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,26 @@
+import { Router, Request, Response } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { PostgresCategoriesRepository } from "../repositories/PostgresCategoriesRepositort";
+import { CreateCategoryService } from "../services/CreateCategoryService";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request: Request, response: Response) => {
+  const { name, description } = request.body;
+
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.status(200).json({ all });
+});
+
+export { categoriesRoutes };
