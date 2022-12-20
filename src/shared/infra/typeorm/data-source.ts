cb21bc6140c83899630444eb514a22fe28e1eb07
bruno3du <bruno.139737@gmail.com>
2022-12-19 22:31:19 -0300

    fixing execute migration typeorm for the one accept path absolute

--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -6,8 +6,6 @@ import { DataSource } from "typeorm";
 import { CreateCategories1667743194366 } from "./migrations/1667743194366-CreateCategories";
 import { CreateSpecifications1667747175244 } from "./migrations/1667747175244-CreateSpecifications";
 import { CreateUsers1668131356621 } from "./migrations/1668131356621-CreateUsers";
-import { AlterUserDeleteUsername1668260438242 } from "./migrations/1668260438242-AlterUserDeleteUsername";
-import { AlterUserAddAvatar1668362300897 } from "./migrations/1668362300897-AlterUserAddAvatar";
 
 export const dataSource = new DataSource({
   type: "postgres",
@@ -23,8 +21,6 @@ export const dataSource = new DataSource({
     CreateCategories1667743194366,
     CreateSpecifications1667747175244,
     CreateUsers1668131356621,
-    AlterUserDeleteUsername1668260438242,
-    AlterUserAddAvatar1668362300897,
   ],
   subscribers: [],
 });
